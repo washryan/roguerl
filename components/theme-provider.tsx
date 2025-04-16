0a5b6114b8179762998f4f78e77b2575da71a8bf
washryan <washryan.3103@gmail.com>
2025-04-15 21:11:16 -0300

    Penultimo commit do dia

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,10 @@
 "use client"
 
-import * as React from "react"
+import type * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+
+type ThemeProviderProps = React.PropsWithChildren<NextThemeProviderProps>
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
